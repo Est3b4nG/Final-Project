e4e8819e4c3d0cbe071d86851d1b1924d324ddeb
Est3b4nG <104792566+Est3b4nG@users.noreply.github.com>
2022-11-29 10:32:19 +0100

    Update background.py

--- background.py
+++ background.py
@@ -3,6 +3,9 @@ Created by (Esteban Gómez) in  ${2022}
 """
 
 import random
+import pyxel
+
+
 class Clouds:
     "Everything related to the clouds"
 
@@ -12,10 +15,15 @@ class Clouds:
 
         self.image=(1, 32, 80, 45, 35, 0)
 
-    def move(self, width, height, position_y):
-        self.position_y+= 2
+
+    def update(self, width, height, position_y):
+        self.position_y += 2
         if self.position_y > height:
             self.position_y = position_y
         if self.position_y < 0 - self.image[4]:
             self.position_x = random.randint(0, width)
+
+    def draw(self):
+        pyxel.blt(self.position_x, self.position_y, *self.image)
+
 
