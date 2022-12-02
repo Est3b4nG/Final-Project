4265ba713bd01423d5601f0cd9cbe1f231bb108f
Est3b4nG <104792566+Est3b4nG@users.noreply.github.com>
2022-12-02 20:15:33 +0100

    Update background.py

--- background.py
+++ background.py
@@ -1,9 +1,46 @@
-"""
-Created by (Esteban Gómez) in  ${2022}
-"""
 
 import random
 import pyxel
+
+
+class Background:
+    def __init__(self, position_1x, position_1y):
+        self.height = 255
+        self.position_1x = position_1x
+        self.position_1y = position_1y
+        self.position_2x= position_1x
+        self.position_2y = position_1y - 100
+        self.position_3x = position_1x
+        self.position_3y = position_1y - 200
+        self.position_4x= position_1x
+        self.position_4y = position_1y - 300
+
+    def update(self):
+        if self.position_1y < self.height:
+            self.position_1y += 0.5
+        if self.position_2y < self.height:
+            self.position_2y += 0.5
+        if self.position_2y == self.height:
+            self.position_2y = -100
+        if self.position_3y < self.height:
+            self.position_3y += 0.5
+        if self.position_3y == self.height:
+            self.position_3y = -100
+        if self.position_3y < -100:
+            self.position_3x = random.randint(-50 , self.height//4)
+        if self.position_4y < self.height:
+            self.position_4y += 0.5
+        if self.position_4y == self.height:
+            self.position_4y = -100
+
+
+
+
+    def draw(self):
+        pyxel.blt(self.position_1x, self.position_1y, 2, 0, 0, 255, 260, 0)
+        pyxel.blt(self.position_2x, self.position_2y, 2, 0, 0, 255, 100, 0)
+        pyxel.blt(self.position_3x, self.position_3y, 0, 0, 0, 175, 79, 0)
+        pyxel.blt(self.position_4x, self.position_4y, 2, 0, 0, 255, 100, 0)
 
 
 class Clouds:
@@ -25,5 +62,8 @@ class Clouds:
 
     def draw(self):
         pyxel.blt(self.position_x, self.position_y, *self.image)
+
+
+
 
 
