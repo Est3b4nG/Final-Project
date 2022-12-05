e8b7e130a261f521449eb3ec258936550d0111fd
Est3b4nG <104792566+Est3b4nG@users.noreply.github.com>
2022-12-05 17:07:56 +0100

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -3,6 +3,7 @@ Created by (Esteban Gómez) in  ${2022}
 """
 import pyxel
 import random
+import time
 
 class Enemy1:
     #We bring the values of position x and y from the board
@@ -21,7 +22,6 @@ class Enemy1:
         self.enemy1_bullet_list= []
 
 
-
     def update(self, width, height):
         # the position in y will be equal to the movement per frame (2 (velocity in which it is going to move)),
         # and we multiply it by the direction in order to determine if it is going up or down
@@ -38,7 +38,7 @@ class Enemy1:
             self.enemy1_bullet_list.append(enemybullets)
 
         for i in range(len(self.enemy1_bullet_list) - 1, -1, -1):
-            self.enemy1_bullet_list[i].update()
+            self.enemy1_bullet_list[i].update(1)
             if self.enemy1_bullet_list[i].position_y < 0 or self.lives <= 0:
                 del(self.enemy1_bullet_list[i])
 
@@ -63,7 +63,7 @@ class Enemy1:
             for bullet in self.enemy1_bullet_list:
                 bullet.draw()
 
-class Bombardier:
+class SuperBombardier:
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -72,6 +72,10 @@ class Bombardier:
         self.x = random.uniform(0, self.width / 4)
         self.y= height-8
         self.puntuation = 0
+        self.superbombardier_bullet_list=[]
+
+        self.time_shot_start = time.time()
+        self.time_shot_end = time.time()
 
 
     def update(self, width, height):
@@ -81,9 +85,29 @@ class Bombardier:
         if self.lives<=0:
             self.puntuation = 5
 
+        self.time_shot_end = time.time()
+        print(int(self.time_shot_end - self.time_shot_start))
+        if self.y == ((height // 2) - self.image[4]) and int(self.time_shot_end-self.time_shot_start) % 5 == 0:
+            for bullet in range(5):
+                enemybullets = Enemies_bullets(self.x, self.y)
+                self.superbombardier_bullet_list.append(enemybullets)
+        if self.time_shot_end==6:
+            self.time_shot_start = 0
+
+
+
+        for i in range(len(self.superbombardier_bullet_list) - 1, -1, -1):
+            self.superbombardier_bullet_list[i].update(4)
+            if self.superbombardier_bullet_list[i].position_y < 0 or self.lives <= 0:
+                del(self.superbombardier_bullet_list[i])
+
+
+
     def draw(self):
         if self.lives > 0:
             pyxel.blt(self.x, self.y, *self.image)
+        for bullet in self.superbombardier_bullet_list:
+            bullet.draw()
 
 
 class Explosion:
@@ -100,13 +124,21 @@ class Explosion:
 class Enemies_bullets:
     """These are the bullets of the player"""
     def __init__(self, position_x, position_y):
-        self.position_x = position_x + 5
-        self.position_y = position_y
+        self.position_x = position_x + 25
+        self.position_y = position_y + 15
 
         self.image=(0, 192, 240, 4, 4, 0)
 
-    def update(self):
-        self.position_y = self.position_y + 3
+    def update(self, type):
+        if type==1:
+            self.position_y = self.position_y + 3
+
+        if type==4:
+            self.position_y += 3
+            direction= [-3, 0, 3]
+            self.position_x= self.position_x + random.choice(direction)
 
     def draw(self):
-        pyxel.blt(self.position_x, self.position_y, *self.image)
+        pyxel.blt(self.position_x + (self.image[3]), self.position_y + (self.image[4]), *self.image)
+
+
