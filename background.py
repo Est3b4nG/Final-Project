b5dd51b60fcbbc4448e496fbc87ff978c8b7f2f2
Est3b4nG <104792566+Est3b4nG@users.noreply.github.com>
2022-12-12 18:13:20 +0100

    Update background.py

--- background.py
+++ background.py
@@ -1,67 +1,38 @@
-
-import random
+"""
+Created by (Esteban Gómez) in  ${2022}
+"""
 import pyxel
 
-
 class Background:
-    def __init__(self, position_1x, position_1y):
+    def __init__(self, position_x, position_y):
         self.height = 255
-        self.position_1x = position_1x
-        self.position_1y = position_1y
-        self.position_2x= position_1x
-        self.position_2y = position_1y - 100
-        self.position_3x = position_1x
-        self.position_3y = position_1y - 200
-        self.position_4x= position_1x
-        self.position_4y = position_1y - 300
+        self.width = 255
+        self.position_x = position_x
+        self.position_island_y = position_y
+        self.position_y_list=[]
+        self.position_clouds_y=position_y
+        for image in range(4):
+            self.position_clouds_y -= 100
+            self.position_y_list.append(self.position_clouds_y)
 
     def update(self):
-        if self.position_1y < self.height:
-            self.position_1y += 0.5
-        if self.position_2y < self.height:
-            self.position_2y += 0.5
-        if self.position_2y == self.height:
-            self.position_2y = -100
-        if self.position_3y < self.height:
-            self.position_3y += 0.5
-        if self.position_3y == self.height:
-            self.position_3y = -100
-        if self.position_3y < -100:
-            self.position_3x = random.randint(-50 , self.height//4)
-        if self.position_4y < self.height:
-            self.position_4y += 0.5
-        if self.position_4y == self.height:
-            self.position_4y = -100
-
-
-
+        if self.position_island_y < self.height:
+            self.position_island_y += 0.5
+        for i in range(len(self.position_y_list)):
+            if self.position_y_list[i] < self.height:
+                self.position_y_list[i] += 0.5
+            if self.position_y_list[i] >= self.height:
+                self.position_y_list[i] = -100
 
     def draw(self):
-        pyxel.blt(self.position_1x, self.position_1y, 2, 0, 0, 255, 260, 0)
-        pyxel.blt(self.position_2x, self.position_2y, 2, 0, 0, 255, 100, 0)
-        pyxel.blt(self.position_3x, self.position_3y, 0, 0, 0, 175, 79, 0)
-        pyxel.blt(self.position_4x, self.position_4y, 2, 0, 0, 255, 100, 0)
-
-
-class Clouds:
-    "Everything related to the clouds"
-
-    def __init__(self, position_x, position_y):
-        self.position_x = position_x
-        self.position_y = position_y
-
-        self.image=(1, 32, 80, 45, 35, 0)
-
-
-    def update(self, width, height, position_y):
-        self.position_y += 2
-        if self.position_y > height:
-            self.position_y = position_y
-        if self.position_y < 0 - self.image[4]:
-            self.position_x = random.randint(0, width)
-
-    def draw(self):
-        pyxel.blt(self.position_x, self.position_y, *self.image)
+        if self.position_island_y < self.height:
+            pyxel.blt(self.position_x, self.position_island_y, 2, 0, 0, 255, 255, 0)
+        for i in range(0, len(self.position_y_list)):
+            pyxel.blt(self.position_x, self.position_y_list[i], 2, 0, 0, 255, 100, 0)
+            if i == 3:
+                if self.position_y_list[i] <= -100:
+                    self.position_x_island2= pyxel.rndi(-50, self.width)
+                pyxel.blt(self.position_x_island2, self.position_y_list[i], 0, 0, 0, 210, 100, 0)
 
 
 
