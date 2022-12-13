9622858b87dd6bbbac41eff17870009a92a8873c
Est3b4nG <104792566+Est3b4nG@users.noreply.github.com>
2022-12-13 23:58:13 +0100

    Update board.py

--- board.py
+++ board.py
@@ -25,6 +25,8 @@ class Board:
         self.start_game=False
         self.puntuation= 0
         self.highest_puntuation=highest_puntuation
+        if self.highest_puntuation < self.puntuation:
+            self.highest_puntuation = self.puntuation
 
         #This initialize pyxel
         pyxel.init(self.width, self.height, title="1942")
@@ -38,9 +40,6 @@ class Board:
 
      #   self.bullets= Bullets(self.player.x, self.player.y)
         self.background= Background(0,0)
-
-        self.start_time = time.time()
-        self.end_time = time.time()
         self.list_enemies=[]
         self.list_explosions= []
         self.Game_Over = False
@@ -66,8 +65,8 @@ class Board:
 
 
         self.list_SuperBombardier=[]
-        superbombardier= SuperBombardier(self.width,self.height)
-        self.list_SuperBombardier.append(superbombardier)
+        #superbombardier= SuperBombardier(self.width,self.height)
+        #self.list_SuperBombardier.append(superbombardier)
         self.list_enemies.append(self.list_SuperBombardier)
 
         self.list_Bombardier = []
@@ -76,6 +75,9 @@ class Board:
         self.list_enemies.append(self.list_Bombardier)
 
         self.start_time_collision = time.time()
+
+        self.time_of_appearence = time.time()
+        self.initial_frame = pyxel.frame_count
         #Runs the game, always ín the last part of the init
         pyxel.run(self.update,self.draw)
 
@@ -92,69 +94,93 @@ class Board:
                     self.scene = 2
             if self.scene == 2:
                 self.background.update()
-                if pyxel.btnp(pyxel.KEY_L):    #If the person press space, the game starts
+                if pyxel.btnp(pyxel.KEY_L):    #If the person press L, the game starts
                     self.start_game = True           #Makes the game start
+                    self.time_of_appearence = time.time()
+                    self.initial_frame= pyxel.frame_count
                 if self.start_game == True:
                     self.player.update(self.width, self.height)
 
-                    if self.player.lives > 0: #If the player have more than 0 lives, it will make the enemies 1 move
+                    if time.time() - self.time_of_appearence > 5:
 
-                        for enemies in self.list_enemies:
-                            for enemy in enemies:
-                                if enemy.lives >= 0:
-                                    enemy.update(self.width,self.height)    # the height of the screen, the place of appearence
+                        if self.player.lives > 0: #If the player have more than 0 lives, it will make the enemies 1 move
 
-                                    for bullet in enemy.bullet_list:
-                                        self.hit_time_end = time.time()
-                                        if bullet.position_x > self.player.x and bullet.position_x <= self.player.x + \
-                                            self.player.image[3] and bullet.position_y > self.player.y and \
-                                            bullet.position_y <= self.player.y + self.player.image[4]:
-                                            self.player.lives -= 1
-                                            enemy.bullet_list.remove(bullet)
+                            for enemies in self.list_enemies:
+                                for enemy in enemies:
+                                    if enemy.lives >= 0:
+                                        enemy.update(self.width,self.height)    # the height of the screen, the place of appearence
 
+                                        for bullet in enemy.bullet_list:
+                                            self.hit_time_end = time.time()
+                                            if bullet.position_x > self.player.x and bullet.position_x <= self.player.x + \
+                                                self.player.image[3] and bullet.position_y > self.player.y and \
+                                                bullet.position_y <= self.player.y + self.player.image[4] and \
+                                                    self.player.image == [0, 208, 240, 16, 16, 0]:
+                                                self.player.lives -= 1
+                                                enemy.bullet_list.remove(bullet)
 
-                                if self.player.x + self.player.image[3] > enemy.x and enemy.x + enemy.image[3] > \
-                                        self.player.x and self.player.y + self.player.image[4] > enemy.y and enemy.y + \
-                                        enemy.image[4] > self.player.y:
-                                            collision = True
-                                            self.end_time_collision = time.time()
-
-                                            if collision == True:
-
-                                                if self.end_time_collision - self.start_time_collision > 2.0:
-                                                    self.player.lives -= 1
-                                                    enemy.lives -= 1
-                                                    self.start_time_collision = time.time()
+                                                explosion = Explosion(self.player.x, self.player.y)
+                                                explosion.image= [1,134, 77, 24, 20,0]
+                                                self.list_explosions.append(explosion)
 
 
-                                for bullet in self.player.list_bullets:
-                                    if (bullet.position_x > enemy.x and bullet.position_x < enemy.x + enemy.image[3]) \
-                                        and bullet.position_y > enemy.y and bullet.position_y < enemy.y + enemy.image[4]:
-                                        enemy.lives -= 1
-                                        explosion=Explosion(enemy.x, enemy.y)
+                                    if self.player.x + self.player.image[3] > enemy.x and enemy.x + enemy.image[3] > \
+                                            self.player.x and self.player.y + self.player.image[4] > enemy.y and enemy.y + \
+                                            enemy.image[4] > self.player.y:
+                                                collision = True
+                                                self.end_time_collision = time.time()
+
+                                                if collision == True:
+
+                                                    if self.end_time_collision - self.start_time_collision > 2.0:
+                                                        self.player.lives -= 1
+                                                        enemy.lives -= 1
+                                                        self.start_time_collision = time.time()
+                                                        explosion = Explosion(self.player.x, self.player.y)
+                                                        explosion.image = [1, 134, 77, 24, 20, 0]
+                                                        self.list_explosions.append(explosion)
+
+
+                                    for bullet in self.player.list_bullets:
+                                        if (bullet.position_x > enemy.x and bullet.position_x < enemy.x + enemy.image[3]) \
+                                            and bullet.position_y > enemy.y and bullet.position_y < enemy.y + enemy.image[4]:
+                                            enemy.lives -= 1
+                                            self.puntuation += enemy.puntuation * 100
+                                            self.player.list_bullets.remove(bullet)
+
+                                    if enemy.lives<=0:
+                                        explosion = Explosion(enemy.x, enemy.y)
+                                        if type(enemy)==SuperBombardier:
+                                            explosion.image= [1,75, 3, 70, 47, 0]
+                                        if type(enemy)==Bombardier:
+                                            explosion.image=[1, 84, 71,26 ,24,0]
                                         self.list_explosions.append(explosion)
-                                        self.puntuation += enemy.puntuation * 100
+                                        enemies.remove(enemy)
 
-                    self.end_time = time.time()
 
-                    if int(self.end_time - self.start_time) == 2:
-                        enemy1 = Enemy1(random.randint(0,self.width), -16)
-                        self.list_enemies[0].append(enemy1)
-                        self.start_time= self.end_time
 
-                    if pyxel.frame_count % 200 == 0:
-                        appearence_x = -50
-                        quantity= pyxel.rndi(2,4)
-                        appearence_y = pyxel.rndi(0, self.height//3)
-                        for enemy in range (quantity):
-                            appearence_x -= 10
-                            enemy2 = Enemy2(0 + appearence_x, appearence_y)
-                            self.list_enemies[1].append(enemy2)
 
-                    if pyxel.frame_count % 100 == 0:
-                        bombardier = Bombardier(self.width, self.height)
-                        self.list_enemies [2].append(bombardier)
+                        if pyxel.frame_count % 60 == 0:
+                            enemy1 = Enemy1(random.randint(0,self.width), -16)
+                            self.list_enemies[0].append(enemy1)
 
+
+                        if pyxel.frame_count % 200 == 0:
+                            appearence_x = -50
+                            quantity= pyxel.rndi(2,4)
+                            appearence_y = pyxel.rndi(0, self.height//3)
+                            for enemy in range (quantity):
+                                appearence_x -= 10
+                                enemy2 = Enemy2(0 + appearence_x, appearence_y)
+                                self.list_enemies[1].append(enemy2)
+
+                        if pyxel.frame_count % 100 == 0:
+                            bombardier = Bombardier(self.width, self.height)
+                            self.list_enemies [2].append(bombardier)
+
+                        if (pyxel.frame_count - self.initial_frame) % 3000 == 0:
+                            superbombardier= SuperBombardier(self.width,self.height)
+                            self.list_enemies[3].append(superbombardier)
 
 
                 if self.player.lives <= 0:
@@ -162,14 +188,22 @@ class Board:
                     for list in self.list_enemies:
                         list.clear()
 
+                if pyxel.frame_count - self.initial_frame == 10_000:
+                    self.scene = 4
+
             print(pyxel.frame_count)
 
-        if self.scene == 3:
+        if self.scene == 3 or self.scene == 4:
             self.Game_Over=True
-            if pyxel.btnp(pyxel.KEY_A):
+            if pyxel.btnp(pyxel.KEY_SPACE):
                 self.scene = 1
                 self.player.lives = 3
                 self.Game_Over = False
+                if self.highest_puntuation < self.puntuation:
+                    self.highest_puntuation = self.puntuation
+                self.puntuation = 0
+                self.background = Background(0, 0)
+                self.player = Player(self.width / 2, self.height - 30, 3)
 
 
     def draw(self):
@@ -195,6 +229,9 @@ class Board:
             pyxel.text(10, self.height-10, "Lives:", 0)    #The number of lives
             pyxel.text(40, self.height - 10, str(self.player.lives), 0)
 
+            pyxel.text(60, self.height - 10, "Loops:", 0 )  # The number of lives
+            pyxel.text(90, self.height - 10, str(self.player.number_loops), 0)
+
 
             self.player.draw()
 
@@ -212,4 +249,10 @@ class Board:
             pyxel.cls(0)
             pyxel.text(self.width / 2 - 20, self.height / 2 - 15, "Game Over", pyxel.frame_count % 10)
             pyxel.text(self.width / 2 - 40, self.height / 2 , "Your puntuation was:", pyxel.frame_count % 10)
+            pyxel.text(self.width / 2 - 12, self.height / 2 + 15, str(self.puntuation), pyxel.frame_count % 10)
+
+        if self.scene==4:
+            pyxel.cls(0)
+            pyxel.text(self.width / 2 - 20, self.height / 2 - 15, "You Won!", pyxel.frame_count % 10)
+            pyxel.text(self.width / 2 - 40, self.height / 2, "Your puntuation was:", pyxel.frame_count % 10)
             pyxel.text(self.width / 2 - 12, self.height / 2 + 15, str(self.puntuation), pyxel.frame_count % 10)
